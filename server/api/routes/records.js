6e8f79d0614622d31d49a82ed7b93016ba5dbabc
lisawcoding <sulinwood@gmail.com>
2021-11-24 23:40:32 +0800

    PunchCamera updated

--- server/api/routes/records.js
+++ server/api/routes/records.js
@@ -13,19 +13,31 @@ recordRouter.post("/", verifyAccessToken, (req, res) => {
      Record.find({ employee: req.params.employeeId, date: req.body.date })
           .then((data) => {
                console.log(data);
-               res.json({ msg: "duplicate data", result: data });
+               if(data.length<1) {
+                    res.json("no record")
+                    // Record.create(req.body)
+                    // .then(data=>res.json(data))
+                    // .catch(err=>res.json({error: err}))
+               } else {
+                    res.json("find one record")
+                    // Record.updateOneAndUpdate({employee: req.params.employeeId, date: req.body.date} , req.body)
+                    // .then(data=>res.json(data))
+                    // .catch(err=>res.json({error: err}))
+               }
+               // res.json({ msg: "duplicate data", result: data });
           })
           .catch((err) => res.json({ error: err }));
 });
 // Record.create({ ...req.body, employee: req.params.employeeId })
 //      .then((data) => res.json(data))
 //      .catch((err) => res.json({ error: err }));
+
 //test only
-// recordRouter.get("/", (req, res) => {
-//      Record.find()
-//           .then((data) => res.json(data))
-//           .catch((err) => res.json({ error: err }));
-// });
+recordRouter.get("/", (req, res) => {
+     Record.find()
+          .then((data) => res.json(data))
+          .catch((err) => res.json({ error: err }));
+});
 recordRouter.get("/:date", (req, res) => {
      console.log("/:date", req.params);
      Record.find({ date: req.params.date })
