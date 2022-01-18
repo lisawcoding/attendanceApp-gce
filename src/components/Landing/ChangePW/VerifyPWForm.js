a4f64a438ea5226878ba004ad55094724650e883
tolisacoding <tolisacoding@gmail.com>
2022-01-18 16:36:22 +0800

    add autoFocus

--- src/components/Landing/ChangePW/VerifyPWForm.js
+++ src/components/Landing/ChangePW/VerifyPWForm.js
@@ -2,7 +2,7 @@ const VerifyPWForm = ({submitVerifyToken, inputValue, changeInput, alert, t, dis
     return (
         <form onSubmit={submitVerifyToken}>
             <input type="email" name="email" placeholder={inputValue.email} disabled />
-            <input type="text" name="password" placeholder="password" onChange={changeInput} required />
+            <input type="text" name="password" placeholder="password" onChange={changeInput} required autoFocus/>
             <input type="text" name="password2" placeholder="confirm password" onChange={changeInput} required />
             <textarea type="text" name="token" placeholder={t("verify token")} rows="5" onChange={changeInput} required />
             <input type="submit" value={t("reset password")} disabled={disabled} />
