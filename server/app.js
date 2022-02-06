7a37e8fc864d91b94665a603de8eab5feb7e4ca9
tolisacoding <tolisacoding@gmail.com>
2022-02-06 18:56:00 +0800

    update manifest.json

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 // const mongoose = require("mongoose");
 const connectDB = require("./config/db");
+const path = require("path");
+const app = express();
 // const dotenv = require("dotenv");
 // const UserToken = require("./api/models/UserToken");
 // var cors = require("cors");
 
-const app = express();
 connectDB();
 // dotenv.config();
 // app.use(cors({
@@ -24,19 +25,26 @@ app.use((req, res, next) => {
      next(); // Pass to next layer of middleware
 });
 
+
+
 //init middeware
+// app.use(express.static(__dirname + '../public'));
+// app.use(express.static(path.join(__dirname,"../public")));
+app.use(express.static(path.join(__dirname,"../build")));
+
 app.use(express.json({ extended: false }));
-app.get("/", (req, res) => res.send("Hello World"));
 app.use("/users", require("./api/routes/users"));
 app.use("/users/:id/employees", require("./api/routes/employees"));
 app.use("/users/:id/employees/:employeeId/records", require("./api/routes/records"));
-
-//testing route
 app.get("/usertoken", (req, res) => {
      UserToken.find()
           .then((data) => res.json(data))
           .catch((err) => res.json(err));
 });
+
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "../build", "index.html"))
+})
 
 const PORT = process.env.PORT || 9001;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
